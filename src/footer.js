deb2fd84342bca8c85d369252352a616b9bebcc5
alexoid <alex_zam@hotmail.es>
2020-11-02 19:42:37 -0500

    fis js files

---
+++ src/footer.js
@@ -0,0 +1,16 @@
+const footerPage = () => {
+  const container = document.getElementById('content');
+  const footer = document.createElement('footer');
+  const fpar = document.createElement('p');
+  const fcontact = document.createElement('a');
+
+  fpar.textContent = 'Made by Alexoid.';
+  fcontact.textContent = 'GitHub';
+  fcontact.href = 'https://github.com/Alexoid1';
+
+  footer.appendChild(fpar);
+  footer.appendChild(fcontact);
+  container.appendChild(footer);
+};
+
+export default footerPage;
